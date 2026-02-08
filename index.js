47cad9ca25b59bbdaf49192f42c48edb0a55171c
Abdullah.kamal <abdullahkamal207@gmail.com>
2026-02-08 14:24:18 +0530

    completed project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const myButton = document.getElementById("myButton");
 let element;
 
 function createGrid(x, y) {
-  while(x >= 100 || y >= 100) {
+  while (x >= 100 || y >= 100) {
     x = prompt("Please type correct x: ");
     y = prompt("Please type correct y: ");
     x = Number(x);
@@ -22,27 +22,49 @@ function createGrid(x, y) {
 }
 createGrid(16, 16);
 
-function removeGrid(x, y){
-  for(let i = 0; i < x; i++){
+function removeGrid(x, y) {
+  for (let i = 0; i < x; i++) {
     container.removeChild(container.children[0]);
   }
 }
-let a = 16;
-let b = 16;
+let p = 16;
+let q = 16;
 
 function generateGrid() {
-
-  removeGrid(a, b);
+  removeGrid(p, q);
 
   let x = prompt("x :");
   let y = prompt("y :");
   x = Number(x);
   y = Number(y);
-  
-  a = x;
-  b = y;
+
+  p = x;
+  q = y;
   console.log(x, typeof x);
   console.log(y, typeof y);
 
   createGrid(x, y);
 }
+
+let myBox = document.getElementsByClassName("box");
+
+let r = Math.floor(Math.random() * 255);
+let g = Math.floor(Math.random() * 255);
+let b = Math.floor(Math.random() * 255);
+
+// container.addEventListener("mouseover", (event) => {
+//   event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// })
+
+// container.addEventListener("mouseout", (event) => {
+//   event.target.style.backgroundColor = "white";
+// })
+
+Array.from(container.children).forEach((c) => {
+  c.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  });
+  c.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "white";
+  });
+});
